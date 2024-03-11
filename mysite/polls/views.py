95de2b9a52bba43cc97d2c58f13fb98e36ad1045
ivank <korichivanV@mail.ru>
2024-03-11 18:43:29 +0700

    Mapped out new views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Test view. You`re at the polls index.')
+
+def detail(request, question_id):
+    return HttpResponse(f'You`re currently at the question {question_id}')
+
+def results(request, question_id):
+    return HttpResponse(f'You`re currently at the results of question {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'You`re voting on question {question_id}')
