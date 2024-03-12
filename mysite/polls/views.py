07d1343c335b4416fd458e343279280d39fb3274
ivank <korichivanV@mail.ru>
2024-03-12 19:15:17 +0700

    Removed some of the hardcode

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -11,12 +11,10 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk = question_id)
-    except Question.DoesNotExist:
-        raise Http404('Such question doesnt exist.')
+    question = get_object_or_404(Question, pk=question_id)
     
     return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
     return HttpResponse(f'You`re currently at the results of question {question_id}')
