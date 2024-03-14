556da4a7b4b2c65487db205c89e5f73a6c9d1f3c
ivank <korichivanV@mail.ru>
2024-03-14 20:46:33 +0700

    Made votes view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
@@ -19,5 +21,20 @@ def detail(request, question_id):
 def results(request, question_id):
     return HttpResponse(f'You`re currently at the results of question {question_id}')
 
+
 def vote(request, question_id):
-    return HttpResponse(f'You`re voting on question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay voting form with error message
+        context = {'question': question, 'error_message': 'You didnt select a choice.'}
+
+        return render(request, 'polls/detail.html', context)
+    
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        
+        # HttpResponseRedirect to prevent double-post if a user hits back button
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
