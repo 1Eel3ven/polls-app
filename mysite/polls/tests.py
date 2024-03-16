fb416a0e171f03e671b11a30acefd4205568c2ac
ivank <korichivanV@mail.ru>
2024-03-16 18:31:08 +0700

    Fixed was_published_recently() method bug; Made tests for this method; Fixed mistake with ResultView in polls` URLconf

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from datetime import timedelta
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        '''Testing if was_published_recently() returns False for questions with pub_date set in the future'''
+        time = timezone.now() + timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        '''Testing if was_published_recently() returns False for questions with pub_date older than 1 day'''
+        time = timezone.now() - timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        '''Testing if was_published_recently() returns True for questions with pub_date within last day'''
+        time = timezone.now() - timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
